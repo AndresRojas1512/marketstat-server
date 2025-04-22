b562ddcdce82e0f379afcdc3dd5350d85e04a943
ra22iu62 <rokhas.mikheevaa@student.bmstu.ru>
2025-04-22 18:17:27 +0300

    vertical sline DimEmployer done - migration done

--- src/MarketStat/Program.cs
+++ src/MarketStat/Program.cs
@@ -1,4 +1,25 @@
+using MarketStat.Database.Context;
+using MarketStat.Database.Core.Repositories.Dimensions;
+using MarketStat.Database.Models;
+using MarketStat.Database.Repositories.PostgresRepositories.Dimensions;
+using MarketStat.Services.Dimensions.DimEmployerService;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var conn = builder.Configuration.GetConnectionString("MarketStat")
+    ?? throw new InvalidOperationException("Missing connection string.");
+
+builder.Services
+    .AddDbContext<MarketStatDbContext>(opts => opts.UseNpgsql(conn));
+
+builder.Services
+    .AddScoped<IDbContextFactory, NpgsqlDbContextFactory>();
+
+builder.Services
+    .AddScoped<IDimEmployerRepository, DimEmployerRepository>()
+    .AddScoped<IDimEmployerService, DimEmployerService>();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
