56f68b98ca889953d141109b3e0528a4508b0f29
ra22iu62 <rokhas.mikheevaa@student.bmstu.ru>
2025-05-07 20:56:08 +0300

    DimStandardJobRoleController done

---
+++ src/MarketStat/Controllers/Dimensions/DimStandardJobRoleController.cs
@@ -0,0 +1,89 @@
+using AutoMapper;
+using MarketStat.Common.Dto.MarketStat.Common.Dto.Dimensions.DimStandardJobRole;
+using MarketStat.Services.Dimensions.DimStandardJobRoleService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MarketStat.Controllers.Dimensions;
+
+[ApiController]
+[Route("api/dimstandardjobroles")]
+public class DimStandardJobRoleController : ControllerBase
+{
+    private readonly IDimStandardJobRoleService _dimStandardJobRoleService;
+    private readonly IMapper _mapper;
+
+    public DimStandardJobRoleController(IDimStandardJobRoleService dimStandardJobRoleService, IMapper mapper)
+    {
+        _dimStandardJobRoleService = dimStandardJobRoleService;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<DimStandardJobRoleDto>>> GetStandardJobRoles()
+    {
+        var list = await _dimStandardJobRoleService.GetAllStandardJobRolesAsync();
+        var dtos = _mapper.Map<IEnumerable<DimStandardJobRoleDto>>(list);
+        return Ok(dtos);
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<DimStandardJobRoleDto>> GetStandardJobRole(int id)
+    {
+        try
+        {
+            var jobRole = await _dimStandardJobRoleService.GetStandardJobRoleByIdAsync(id);
+            var dto = _mapper.Map<DimStandardJobRoleDto>(jobRole);
+            return Ok(dto);
+        }
+        catch (Exception ex)
+        {
+            return NotFound(new { Message = ex.Message });
+        }
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<DimStandardJobRoleDto>> PostStandardJobRole(DimStandardJobRoleDto dto)
+    {
+        try
+        {
+            var created =
+                await _dimStandardJobRoleService.CreateStandardJobRoleAsync(dto.StandardJobRoleTitle,
+                    dto.IndustryFieldId);
+            return CreatedAtAction(nameof(GetStandardJobRole), new { id = dto.StandardJobRoleId }, dto);
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(new { Message = ex.Message });
+        }
+    }
+
+    [HttpPut("{id:int}")]
+    public async Task<IActionResult> PutStandardJobRole(int id, DimStandardJobRoleDto dto)
+    {
+        try
+        {
+            await _dimStandardJobRoleService.UpdateStandardJobRoleAsync(id, dto.StandardJobRoleTitle,
+                dto.IndustryFieldId);
+            return NoContent();
+        }
+        catch (Exception ex)
+        {
+            return NotFound(new { Message = ex.Message });
+        }
+    }
+
+    [HttpDelete("{id:int}")]
+    public async Task<ActionResult> DeleteStandardJobRole(int id)
+    {
+        try
+        {
+            await _dimStandardJobRoleService.DeleteStandardJobRoleAsync(id);
+            return NoContent();
+        }
+        catch (Exception ex)
+        {
+            return NotFound(new { Message = ex.Message });
+        }
+    }
+    
+}
