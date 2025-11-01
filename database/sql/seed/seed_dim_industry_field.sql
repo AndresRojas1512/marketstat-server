14db14617d22949eadfb557f7c88b0c1b37b49c2
AndresRojas1512 <andresrojasfcc@gmail.com>
2025-11-01 19:53:51 +0300

    refactor(db): Refactor seed_dim_industry_field.sql

--- database/sql/seed/seed_dim_industry_field.sql
+++ database/sql/seed/seed_dim_industry_field.sql
@@ -1,21 +1,14 @@
 SET search_path = marketstat, public;
 \set ON_ERROR_STOP on
 
-
-
-DROP TABLE IF EXISTS staging_industry_fields_temp;
-
 CREATE TEMP TABLE staging_industry_fields_temp (
     industry_field_code TEXT,
     industry_field_name TEXT
 );
 
+\copy staging_industry_fields_temp(industry_field_code, industry_field_name) FROM '/home/andres/Desktop/7-semester/marketstat/server/database/datasets/dim_industry_field_dataset.csv' WITH (FORMAT CSV, HEADER TRUE, DELIMITER ',');
 
-\set enriched_industry_csv_path '/home/andres/Desktop/6Semester/SoftwareDesign/PPO/database/datasets/dim_industry_field_dataset.csv'
-
-\copy staging_industry_fields_temp(industry_field_code, industry_field_name) FROM '/home/andres/Desktop/6Semester/SoftwareDesign/PPO/database/datasets/dim_industry_field_dataset.csv' WITH (FORMAT CSV, HEADER TRUE, DELIMITER ',');
-
-SELECT count(*) AS staged_rows FROM staging_industry_fields_temp;
+SELECT count(*) FROM staging_industry_fields_temp;
 
 BEGIN;
 
@@ -25,9 +18,11 @@ SELECT
     TRIM(s.industry_field_name)
 FROM
     staging_industry_fields_temp s
-WHERE
-    s.industry_field_code IS NOT NULL AND s.industry_field_name IS NOT NULL
-ON CONFLICT (industry_field_code) DO NOTHING;
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM marketstat.dim_industry_field dif
+    WHERE dif.industry_field_code = TRIM(s.industry_field_code)
+        OR dif.industry_field_name = TRIM(s.industry_field_name)
+);
 
 COMMIT;
-
