d979524e1a7214c650d2d90c1053f3701b4b9a53
AndresRojas1512 <andresrojasfcc@gmail.com>
2025-11-04 15:57:14 +0300

    tests(unit): Implement DimJob unit tests

---
+++ src/MarketStat.Tests/MarketStat.Tests.TestData/Builders/Dimensions/DimJobBuilder.cs
@@ -0,0 +1,53 @@
+using MarketStat.Common.Core.MarketStat.Common.Core.Dimensions;
+
+namespace MarketStat.Tests.TestData.Builders.Dimensions;
+
+public class DimJobBuilder
+{
+    private int _jobId = 0;
+    private string _jobRoleTitle = "Software Developer";
+    private string _standardJobRoleTitle = "Software Engineer";
+    private string _hierarchyLevelName = "Mid-Level";
+    private int _industryFieldId = 1;
+    
+    public DimJobBuilder WithId(int id)
+    {
+        _jobId = id;
+        return this;
+    }
+    
+    public DimJobBuilder WithJobRoleTitle(string title)
+    {
+        _jobRoleTitle = title;
+        return this;
+    }
+    
+    public DimJobBuilder WithStandardJobRoleTitle(string title)
+    {
+        _standardJobRoleTitle = title;
+        return this;
+    }
+    
+    public DimJobBuilder WithHierarchyLevelName(string levelName)
+    {
+        _hierarchyLevelName = levelName;
+        return this;
+    }
+    
+    public DimJobBuilder WithIndustryFieldId(int industryFieldId)
+    {
+        _industryFieldId = industryFieldId;
+        return this;
+    }
+    
+    public DimJob Build()
+    {
+        return new DimJob(
+            _jobId,
+            _jobRoleTitle,
+            _standardJobRoleTitle,
+            _hierarchyLevelName,
+            _industryFieldId
+        );
+    }
+}
---
+++ src/MarketStat.Tests/MarketStat.Tests.TestData/ObjectMothers/Dimensions/DimJobObjectMother.cs
@@ -0,0 +1,37 @@
+using MarketStat.Common.Core.MarketStat.Common.Core.Dimensions;
+using MarketStat.Tests.TestData.Builders.Dimensions;
+
+namespace MarketStat.Tests.TestData.ObjectMothers.Dimensions;
+
+public static class DimJobObjectMother
+{
+    public static DimJob ANewJob() =>
+        new DimJobBuilder()
+            .WithId(0)
+            .WithStandardJobRoleTitle("QA Engineer")
+            .WithIndustryFieldId(1)
+            .Build();
+    
+    public static DimJob AnExistingJob() =>
+        new DimJobBuilder()
+            .WithId(1)
+            .WithStandardJobRoleTitle("Software Engineer")
+            .WithIndustryFieldId(1)
+            .Build();
+    
+    public static DimJob ASecondExistingJob() =>
+        new DimJobBuilder()
+            .WithId(2)
+            .WithStandardJobRoleTitle("Data Analyst")
+            .WithIndustryFieldId(2)
+            .Build();
+    
+    public static IEnumerable<DimJob> SomeJobs()
+    {
+        return new List<DimJob>
+        {
+            AnExistingJob(),
+            ASecondExistingJob()
+        };
+    }
+}
