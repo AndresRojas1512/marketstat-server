4a6d794d68e55916c88d099067548ec9038fd1b2
AndresRojas1512 <andresrojasfcc@gmail.com>
2025-11-16 16:17:28 +0300

    ci/cd: Refactor entrypoint.sh

--- test-infra/docker/runner/entrypoint.sh
+++ test-infra/docker/runner/entrypoint.sh
@@ -3,14 +3,14 @@
 set -e
 
 echo "=== Running Unit Tests ==="
-dotnet test /src/src/MarketStat.Tests/MarketStat.Services.Tests/MarketStat.Services.Tests.csproj -c Release
-dotnet test /src/src/MarketStat.Tests/MarketStat.Repository.Tests/MarketStat.Repository.Tests.csproj -c Release
+dotnet test MarketStat.Tests/MarketStat.Services.Tests/MarketStat.Services.Tests.csproj -c Release
+dotnet test MarketStat.Tests/MarketStat.Repository.Tests/MarketStat.Repository.Tests.csproj -c Release
 
 echo "=== Running Integration Tests ==="
-dotnet test /src/src/MarketStat.Tests/MarketStat.Integration.Tests/MarketStat.Integration.Tests.csproj -c Release
+dotnet test MarketStat.Tests/MarketStat.Integration.Tests/MarketStat.Integration.Tests.csproj -c Release
 
 echo "=== Running E2E Tests ==="
-dotnet test /src/src/MarketStat.Tests/MarketStat.Tests.E2E/MarketStat.Tests.E2E.csproj -c Release
+dotnet test MarketStat.Tests/MarketStat.Tests.E2E/MarketStat.Tests.E2E.csproj -c Release
 
 
 echo "=== All Tests Passed! ==="
