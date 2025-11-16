2d58700797d1436d2356043c073b2ef0e4bd4b89
AndresRojas1512 <andresrojasfcc@gmail.com>
2025-11-16 16:55:43 +0300

    ci/cd: Fix permission issues with truncate

--- test-infra/docker/postgres/01-init.sql
+++ test-infra/docker/postgres/01-init.sql
@@ -29,6 +29,10 @@ GRANT USAGE, CREATE ON SCHEMA marketstat TO marketstat_administrator;
 GRANT USAGE, CREATE ON SCHEMA marketstat TO marketstat_analyst;
 GRANT USAGE ON SCHEMA marketstat TO marketstat_public_guest; -- Guest only needs USAGE
 
+-- Grant role membership to allow full cross-role permissions
+GRANT marketstat_analyst TO marketstat_administrator;
+GRANT marketstat_administrator TO marketstat_analyst;
+
 \echo 'Setting default privileges for tables and sequences'
 
 -- === Privileges for objects created by the API (analyst) ===
