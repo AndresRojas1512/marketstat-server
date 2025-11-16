0988a8cd920c367c9d22c6473b11021dd2d523dd
AndresRojas1512 <andresrojasfcc@gmail.com>
2025-11-16 18:09:08 +0300

    tests: e2e collection

---
+++ src/MarketStat.Tests/MarketStat.Tests.E2E/E2ECollection.cs
@@ -0,0 +1,9 @@
+using MarketStat.Integration.Tests;
+
+namespace MarketStat.Tests.E2E;
+
+[CollectionDefinition("Integration")]
+public class E2ECollection : ICollectionFixture<IntegrationTestFixture>
+{
+    
+}
