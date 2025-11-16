1b872d0bf133f1058ada3e39108b6c3d1c311941
AndresRojas1512 <andresrojasfcc@gmail.com>
2025-11-16 18:29:54 +0300

    ,

--- src/MarketStat.Tests/MarketStat.Integration.Tests/IntegrationTestCollection.cs
+++
@@ -1,9 +0,0 @@
-using Xunit;
-
-namespace MarketStat.Integration.Tests;
-
-[CollectionDefinition("Integration")]
-public class IntegrationTestCollection : ICollectionFixture<IntegrationTestFixture>
-{
-    
-}
